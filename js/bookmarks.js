b89a907a2cbd7a24b0ef7c4b9bd4783c73699ea4
Dimitry <dimitryb@gmail.com>
2011-09-05 15:14:48 -0400

    added location, updated styles, new screenshot

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -7,13 +7,28 @@ window.RecentBookmarks = function() {
 		
 		parseRecent : function(recentBookmarks) {
 			$('#loading').remove();
-			var ul = $('<ul></ul>').appendTo($('body')), date;
+			var ul = $('<ul></ul>').appendTo($('body')), date, html;
 			for (var i=0, l=recentBookmarks.length; i<l; i++) {
 				date = new Date(recentBookmarks[i].dateAdded);
-				ul.append(
-					$('<li></li>').html('<a href="' + recentBookmarks[i].url + '">' + recentBookmarks[i].title + '</a><br><small title="' + date.toISOString() + '">' + jQuery.timeago(date) + '</small>')
-				);
+				html = '<a href="' + recentBookmarks[i].url + '">' + recentBookmarks[i].title + '</a><br>';
+				html += '<small class="date" title="' + date.toISOString() + '">' + jQuery.timeago(date) + '</small><br>';
+				html += '<small class="location" id="location_' + recentBookmarks[i].id + '"></small>';
+				$('<li></li>').html(html).appendTo(ul);
+				RecentBookmarks.getLocation(recentBookmarks[i].id, recentBookmarks[i].parentId);
 			}
+		},
+		
+		getLocation : function(child_id, lookup_id) {
+			chrome.bookmarks.get(lookup_id, function(bookmarks) {
+				if (bookmarks.length > 0 && bookmarks[0].parentId) {
+					var html = bookmarks[0].parentId != '0' ? ' &rarr; ' : '';
+					//html += '<a href="chrome://bookmarks/#' + bookmarks[0].id + '">' + bookmarks[0].title + '</a>';
+					html += bookmarks[0].title;
+					html += $('#location_' + child_id).html();
+					$('#location_' + child_id).html(html);
+					RecentBookmarks.getLocation(child_id, bookmarks[0].parentId);
+				}
+			});
 		}
 		
 	};
